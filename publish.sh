14050ac2603048f3bfa9f2aec7f22e34651edd8e
Khanage <khanage@gmail.com>
2024-04-13 11:04:26 +1000

    Remove push step from publish script

--- publish.sh
+++ publish.sh
@@ -4,14 +4,7 @@ COMMIT_MSG="${1:-Pushing changes}"
 
 cargo build --release --target wasm32-unknown-unknown
 wasm-bindgen --no-typescript --target web \
-    --out-dir ./web/ \
+    --out-dir ../khanage.github.io/games/ \
     --out-name "pong" \
     ./target/wasm32-unknown-unknown/release/pong.wasm
-
-pushd web
-
-git commit -am "$COMMIT_MSG"
-git push
-
-popd
 
